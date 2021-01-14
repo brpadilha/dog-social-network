db4182d4779b365eca2fdd732210bccfe3e6525e
Bruno Padilha <brpadilha.dev@gmail.com>
2021-01-14 13:50:17 -0300

    Estiliza os comentarios das fotos

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -1,5 +1,30 @@
 import React from "react";
+import { UserContext } from "../../Hooks/UserContext";
+import PhotoCommentsForm from "./PhotoCommentsForm";
+import styles from "./PhotoComments.module.css";
 
-export default function PhotoComments() {
-  return <div>Coments</div>;
-}
+const PhotoComments = (props) => {
+  const [comments, setComments] = React.useState(() => props.comments);
+  const commentsSection = React.useRef(null);
+  const { login } = React.useContext(UserContext);
+
+  React.useEffect(() => {
+    commentsSection.current.scrollTop = commentsSection.current.scrollHeight;
+  }, [comments]);
+
+  return (
+    <>
+      <ul ref={commentsSection} className={styles.comments}>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+    </>
+  );
+};
+
+export default PhotoComments;
